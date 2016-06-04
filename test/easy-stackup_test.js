40ac6d1178b61c3887bb95f1e1e93562e274cd50
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-06-04 21:36:55 +0100

    Add test to check for handling of no callback

--- test/easy-stackup_test.js
+++ test/easy-stackup_test.js
@@ -13,4 +13,15 @@ describe('easy-stackup function', function() {
       done()
     })
   })
+  it('should accept options as the second argument', function(done) {
+    easyStackup([], {}, function(error, success) {
+      expect(error).to.not.be.ok
+      expect(success).to.be.ok
+      done()
+    })
+  })
+  it('should fail without a callback', function() {
+    expect(easyStackup.bind(easyStackup, [])).to.throw(TypeError)
+    expect(easyStackup.bind(easyStackup, [], {})).to.throw(TypeError)
+  })
 })
