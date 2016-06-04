0c8bc91aa0c4cfbf15da47143c239d2a3c64a419
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-06-04 22:18:04 +0100

    Add tests for callback return of simpler API

--- test/easy-stackup_test.js
+++ test/easy-stackup_test.js
@@ -7,16 +7,16 @@ var easyStackup = require('../lib/easy-stackup')
 
 describe('easy-stackup function', function() {
   it('should accept and call node style callback', function(done) {
-    easyStackup([], function(error, success) {
+    easyStackup([], function(error, stackup) {
       expect(error).to.not.be.ok
-      expect(success).to.be.ok
+      expect(stackup).to.be.ok
       done()
     })
   })
   it('should accept options as the second argument', function(done) {
-    easyStackup([], {}, function(error, success) {
+    easyStackup([], {}, function(error, stackup) {
       expect(error).to.not.be.ok
-      expect(success).to.be.ok
+      expect(stackup).to.be.ok
       done()
     })
   })
@@ -26,9 +26,36 @@ describe('easy-stackup function', function() {
   })
   it('should accept a layer with a gerber string and filename', function(done) {
     var layers = [{gerber:'', filename:''}]
-    easyStackup(layers, function(error, success) {
+    easyStackup(layers, function(error, stackup) {
       expect(error).to.not.be.ok
-      expect(success).to.be.ok
+      expect(stackup).to.be.ok
+      done()
+    })
+  })
+  it('should accept a layer with a gerber string and layerType', function(done) {
+    var layers = [{gerber:'', layerType:'tcu'}]
+    easyStackup(layers, function(error, stackup) {
+      expect(error).to.not.be.ok
+      expect(stackup).to.be.ok
+      done()
+    })
+  })
+  it('should error on an invalid layerType', function(done) {
+    var layers = [{gerber:'', filename: '', layerType:''}]
+    easyStackup(layers, function(error, stackup) {
+      expect(error).to.be.ok
+      expect(stackup).to.not.be.ok
+      done()
+    })
+  })
+  it('should callback with top, bottom and layer array', function(done) {
+    var layers = [{gerber:'', filename: '', layerType:''}]
+    easyStackup(layers, function(error, stackup) {
+      expect(error).to.not.be.ok
+      expect(stackup).to.be.ok
+      expect(stackup).to.be.an('object')
+      expect(stackup).to.have.all.keys('top', 'bottom', 'layers')
+      expect(stackup.layers).to.be.an.instanceOf('Array')
       done()
     })
   })
