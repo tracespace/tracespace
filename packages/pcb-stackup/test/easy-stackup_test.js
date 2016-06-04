1e8368bab6e0e6c576a3f9376d222a1e8995fb83
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-06-04 21:04:58 +0100

    Conform to lint as much as makes sense

--- packages/pcb-stackup/test/easy-stackup_test.js
+++ packages/pcb-stackup/test/easy-stackup_test.js
@@ -6,9 +6,9 @@ var expect = require('chai').expect
 var easyStackup = require('../lib/easy-stackup')
 
 describe('easy-stackup function', function() {
-  it('should return a promise', function () {
-    var result = easyStackup([]);
-    expect(result).to.have.property('then');
-    expect(result.then).to.be.a('function');
-  });
-});
+  it('should return a promise', function() {
+    var result = easyStackup([])
+    expect(result).to.have.property('then')
+    expect(result.then).to.be.a('function')
+  })
+})
