2819e9bcb01f1437c39afe8a170d4f35340455cd
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-06-05 03:04:14 +0100

    Test for plotAsOutline option

--- test/easy-stackup_test.js
+++ test/easy-stackup_test.js
@@ -80,6 +80,14 @@ describe('easy-stackup function', function() {
       done()
     })
   })
+  it('respects plotAsOutline option', function(done) {
+    var layers = [{gerber:emptyGerber, layerType:'tcu', options:{plotAsOutline:true}}]
+    easyStackup(layers, function(error, stackup) {
+      expect(error).to.not.be.ok
+      expect(stackup.layers[0].options.plotAsOutline).to.be.true
+      done()
+    })
+  })
   it('handles multiple layers', function(done) {
     var layers = [{gerber:emptyGerber, layerType:'fcu'}, {gerber:emptyGerber, layerType:'tcu'}]
     easyStackup(layers, function(error, stackup) {
