65b08ec9a4dbf250f4b00d99c23a1e7187a8ac87
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-07-23 01:57:25 +0100

    Lint list-boards.js

--- packages/pcb-stackup/integration/list-boards.js
+++ packages/pcb-stackup/integration/list-boards.js
@@ -3,36 +3,37 @@ var fs = require('fs')
 var path = require('path')
 
 var BOARDS = []
-var boardFolders = fs.readdirSync(__dirname + '/boards')
+var boardFolders = fs.readdirSync(path.join(__dirname, 'boards'))
 var count = boardFolders.length
 
 var done = function() {
   if (--count === 0) {
-    fs.writeFileSync(__dirname + '/boards.json', JSON.stringify(BOARDS, null, 2))
+    fs.writeFileSync(path.join(__dirname, 'boards.json'), JSON.stringify(BOARDS, null, 2))
   }
 }
 
 boardFolders.forEach(function(board) {
-  fs.readdir(__dirname + '/boards/' + board, function(board, error, files) {
+  fs.readdir(path.join(__dirname, 'boards', board), function(board, error, files) {
     if (error) {
       throw error
     }
 
-    var dir = 'boards/' + board + '/'
-
+    var dir = path.join('boards', board)
     var options = {}
     var optionsExists = files.findIndex(function(file) {return file === 'options.json'}) >= 0
 
     if (optionsExists) {
-      options = require(__dirname + '/' + dir + 'options.json')
+      options = require(path.join(__dirname, dir, 'options.json'))
     }
 
     var layers = files.map(function(file) {
       var layerOptions
+
       if (options != null && options.layers != null) {
         layerOptions = options.layers[file]
       }
-      return {path: dir + file, options: layerOptions}
+
+      return {path: path.join(dir, file), options: layerOptions}
     })
 
     if (options != null) {
