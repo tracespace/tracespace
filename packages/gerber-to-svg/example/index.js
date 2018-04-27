35411f7ce64b10242cf47236382ad26316699970
Mike Cousins <mike@cousins.io>
2018-04-27 01:24:22 -0400

    chore: Add gerber-to-svg example and publish svgs to npm

---
+++ packages/gerber-to-svg/example/index.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const pump = require('pump')
+const shortId = require('shortid')
+
+const gerberToSvg = require('..')
+
+const GERBERS_DIR = path.join(__dirname, '../../fixtures/boards/arduino-uno')
+const OUT_DIR = __dirname
+
+const GERBER_PATHS = [
+  path.join(GERBERS_DIR, 'arduino-uno.cmp'),
+  path.join(GERBERS_DIR, 'arduino-uno.drd'),
+  path.join(GERBERS_DIR, 'arduino-uno.gko'),
+  path.join(GERBERS_DIR, 'arduino-uno.plc'),
+  path.join(GERBERS_DIR, 'arduino-uno.sol'),
+  path.join(GERBERS_DIR, 'arduino-uno.stc'),
+  path.join(GERBERS_DIR, 'arduino-uno.sts')
+]
+
+GERBER_PATHS.forEach(filename => {
+  const name = path.basename(filename)
+  const out = path.join(OUT_DIR, `${name}.svg`)
+
+  const file = fs.createReadStream(filename)
+  const options = {id: shortId.generate()}
+
+  const source = gerberToSvg(file, options)
+  const destination = fs.createWriteStream(out)
+
+  pump(source, destination, error => {
+    if (error) return console.error(`Error rendering ${name}`, error)
+    console.log(`Wrote: ${out}`)
+  })
+})
