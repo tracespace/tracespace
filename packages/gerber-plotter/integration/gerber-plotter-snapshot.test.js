4b46b19bbf0008c8d334ef657e73eecb8419f000
Mike Cousins <mike@cousins.io>
2018-06-15 21:25:25 -0400

    test(integration): Add gerber-plotter snapshots ahead of import

---
+++ packages/gerber-plotter/integration/gerber-plotter-snapshot.test.js
@@ -0,0 +1,35 @@
+// gerber-plotter render snapshot tests
+'use strict'
+
+const snapshot = require('snap-shot-it')
+
+const {getGerberSpecs, getBoards} = require('@tracespace/fixtures')
+const getResults = require('./get-results')
+
+const SUITES = [
+  ...getGerberSpecs.sync(),
+  ...getBoards.sync().filter(b => !b.skipSnapshot)
+]
+
+describe(`gerber-plotter :: integration`, function () {
+  before(function () {
+    if (process.env.INTEGRATION !== '1') return this.skip()
+  })
+
+  SUITES.forEach(suite => describe(suite.name, function () {
+    const specs = suite.specs || suite.layers
+    let suiteResults
+
+    before(function (done) {
+      getResults(suite, (error, results) => {
+        if (error) return done(error)
+        suiteResults = results
+        done()
+      })
+    })
+
+    specs.forEach((spec, i) => it(`renders ${spec.name}`, function () {
+      snapshot(suiteResults.specs[i].render)
+    }))
+  }))
+})
---
+++ packages/gerber-plotter/integration/get-results.js
@@ -0,0 +1,49 @@
+// runs gerber-parser on the gerber spec and board suites
+'use strict'
+
+const concat = require('concat-stream')
+const pump = require('pump')
+const runParallel = require('run-parallel')
+const runWaterfall = require('run-waterfall')
+const toReadable = require('to-readable-stream')
+
+const debug = require('debug')('tracespace/gerber-plotter/integration')
+const gerberParser = require('gerber-parser')
+const gerberPlotter = require('gerber-plotter')
+
+module.exports = function getSuiteResults (suite, done) {
+  debug(`Rendering suite ${suite.name}`)
+
+  const specs = suite.specs || suite.layers
+  const specTasks = specs.map(spec => next => renderSpec(spec, next))
+
+  runWaterfall(
+    [
+      next => runParallel(specTasks, next),
+      (specs, next) => next(null, Object.assign(suite, {specs}))
+    ],
+    done
+  )
+}
+
+function renderSpec (spec, done) {
+  debug(`Rendering ${spec.category} - ${spec.name}`)
+
+  const renderOptions = Object.assign({
+    plotAsOutline: spec.type === 'out'
+  }, spec.options)
+
+  let render
+  const source = toReadable(spec.source)
+  const parser = gerberParser(renderOptions)
+  const plotter = gerberPlotter(renderOptions)
+  const dest = concat(result => (render = result))
+
+  runWaterfall(
+    [
+      next => pump(source, parser, plotter, dest, next),
+      next => next(null, Object.assign({render}, spec))
+    ],
+    done
+  )
+}
