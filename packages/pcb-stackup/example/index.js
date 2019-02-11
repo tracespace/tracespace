3cd0ad209e0a686b4d048735f53ea4f69e39366f
Mike Cousins <mike@cousins.io>
2019-02-11 08:59:44 -0500

    fixup: Rewrite pcb-stackup example with Promises

--- packages/pcb-stackup/example/index.js
+++ packages/pcb-stackup/example/index.js
@@ -2,16 +2,16 @@
 
 const fs = require('fs')
 const path = require('path')
-const runParallel = require('run-parallel')
-const runWaterfall = require('run-waterfall')
-const pcbStackup = require('..')
+const {promisify} = require('util')
+const pcbStackup = require('pcb-stackup')
+
+const writeFile = promisify(fs.writeFile)
 
 const GERBERS_DIR = path.join(__dirname, '../../fixtures/boards/arduino-uno')
-
 const TOP_OUT = path.join(__dirname, 'arduino-uno-top.svg')
 const BOTTOM_OUT = path.join(__dirname, 'arduino-uno-bottom.svg')
 
-const GERBER_PATHS = [
+const GERBER_FILENAMES = [
   'arduino-uno.cmp',
   'arduino-uno.drd',
   'arduino-uno.gko',
@@ -19,28 +19,25 @@ const GERBER_PATHS = [
   'arduino-uno.sol',
   'arduino-uno.stc',
   'arduino-uno.sts',
-].map(filename => path.join(GERBERS_DIR, filename))
+]
 
-runWaterfall([renderStackup, writeStackup], error => {
-  if (error) return console.error('Error rendering stackup', error)
+renderStackup()
+  .then(writeStackup)
+  .then(() => console.log(`Wrote:\n  ${TOP_OUT}\n  ${BOTTOM_OUT}`))
+  .catch(error => console.error('Error rendering stackup', error))
 
-  console.log(`Wrote:\n  ${TOP_OUT}\n  ${BOTTOM_OUT}`)
-})
-
-function renderStackup(done) {
-  const layers = GERBER_PATHS.map(filename => ({
+function renderStackup() {
+  const layers = GERBER_FILENAMES.map(filename => ({
     filename,
-    gerber: fs.createReadStream(filename),
+    gerber: fs.createReadStream(path.join(GERBERS_DIR, filename)),
   }))
 
-  pcbStackup(layers, done)
+  return pcbStackup(layers)
 }
 
-function writeStackup(stackup, done) {
-  const tasks = [
-    next => fs.writeFile(TOP_OUT, stackup.top.svg, next),
-    next => fs.writeFile(BOTTOM_OUT, stackup.bottom.svg, next),
-  ]
-
-  runParallel(tasks, done)
+function writeStackup(stackup) {
+  return Promise.all([
+    writeFile(TOP_OUT, stackup.top.svg),
+    writeFile(BOTTOM_OUT, stackup.bottom.svg),
+  ])
 }
