cb04d0778d3e1e52c720b4d9d1742def0b04ebd5
Mike Cousins <mike@tracespace.io>
2019-03-09 11:58:04 -0500

    ci(s3): Clean stale files during s3 publish

--- scripts/publish-to-s3.js
+++ scripts/publish-to-s3.js
@@ -9,9 +9,9 @@ const mime = require('mime')
 const omit = require('lodash/omit')
 
 const {AWS_S3_REGION, AWS_S3_BUCKET} = process.env
-const [localPath, publishPath = ''] = process.argv.slice(2)
+const args = process.argv.slice(2)
 
-const USAGE = 'node ./path/to/publish-to-s3.js <local_path> [publish_path]'
+const USAGE = 'node ./scripts/publish-to-s3.js <local_path:publish_path>...'
 
 const AWS_S3_ACL = 'public-read'
 const AWS_S3_DEFAULT_CACHE = ['public', 'max-age=31536000']
@@ -26,14 +26,32 @@ assert(
   'Expected AWS_S3_BUCKET to be defined'
 )
 
-assert(typeof localPath === 'string', USAGE)
+assert(args.length > 0, USAGE)
 
 const s3 = new Aws.S3({apiVersion: '2006-03-01', region: AWS_S3_REGION})
-const uploadDir = path.resolve(process.cwd(), localPath)
 
-upload()
+const publishTargets = args.map(s => {
+  const [localDir, publishPath] = s.split(':')
+  assert(typeof localDir === 'string' && typeof publishPath === 'string', USAGE)
+  return [path.join(process.cwd(), localDir), publishPath]
+})
+
+console.log(`Deploying to s3://${AWS_S3_BUCKET}`)
+
+Promise.all([getObjectsToUpload(), getExistingObjects()])
+  .then(([uploads, existing]) => {
+    assert(uploads.length > 0, 'No files to upload found')
+    const removals = getObjectsToRemove(uploads, existing)
+
+    console.log(`Uploading ${uploads.length} objects`)
+
+    return Promise.all(uploads.map(makeUploadRequest)).then(() => {
+      console.log(`Uploads successful; removing ${removals.length} old files`)
+      return Promise.all(removals.map(makeRemoveRequest))
+    })
+  })
   .then(() => {
-    console.log('Publish successful')
+    console.log('Removals successful; publish done')
     process.exit(0)
   })
   .catch(error => {
@@ -41,12 +59,24 @@ upload()
     process.exit(1)
   })
 
-function upload() {
-  console.log(`uploading ${uploadDir} to s3://${AWS_S3_BUCKET}/${publishPath}`)
-
-  return glob(uploadDir, {absolute: true, dot: true}).then(files =>
-    Promise.all(files.map(buildUploadParams).map(makeUploadRequest))
+function getObjectsToUpload() {
+  const localDirs = publishTargets.map(([local]) => local)
+  return glob(localDirs, {absolute: true, dot: true}).then(files =>
+    files.map(buildUploadParams)
   )
+}
+
+function getExistingObjects() {
+  return s3
+    .listObjectsV2({Bucket: AWS_S3_BUCKET})
+    .promise()
+    .then(response =>
+      response.Contents.map(f => ({Bucket: AWS_S3_BUCKET, Key: f.Key}))
+    )
+}
+
+function getObjectsToRemove(uploads, existing) {
+  return existing.filter(e => !uploads.some(u => u.Key === e.Key))
 }
 
 function makeUploadRequest(params) {
@@ -55,7 +85,17 @@ function makeUploadRequest(params) {
   return s3.putObject(params).promise()
 }
 
+function makeRemoveRequest(params) {
+  console.log('Delete:', params)
+
+  return s3.deleteObject(params).promise()
+}
+
 function buildUploadParams(filepath) {
+  const target = publishTargets.find(([local]) => filepath.startsWith(local))
+  assert(target, `Couldn't find directory for ${filepath}`)
+
+  const [uploadDir, publishPath] = target
   const relPath = path.relative(uploadDir, filepath)
   const outPath = path.join(publishPath, relPath)
 
