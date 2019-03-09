3c12904d05b281563040ff4a53914c056c0e0698
Mike Cousins <mike@tracespace.io>
2019-03-08 23:58:55 -0500

    fixup: Address yargs deprecations

--- packages/pcb-stackup/example/index.js
+++ packages/pcb-stackup/example/index.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs')
 const path = require('path')
-const promisify = require('pify')
+const {promisify} = require('util')
 const pcbStackup = require('pcb-stackup')
 
 const writeFile = promisify(fs.writeFile)
