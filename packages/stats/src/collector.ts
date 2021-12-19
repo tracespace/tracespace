650bfe7922b3b96cc72d8dd70bbd5241ea5f7d0a
KN4CK3R <admin@oldschoolhack.me>
2021-12-19 22:03:15 +0000

    Do not use instanceof.

--- packages/stats/src/collector.ts
+++ packages/stats/src/collector.ts
@@ -1,4 +1,4 @@
-import {Root, ToolShape, ToolDefinition, Circle, ToolChange, InterpolateMode} from '@tracespace/parser'
+import * as Parser from '@tracespace/parser'
 import {Stats} from './types'
 
 /**
@@ -8,7 +8,7 @@ import {Stats} from './types'
  */
 export interface StatsCollector {
   /** Collect stats of a parsed AST */
-  collect(root: Root): Stats
+  collect(root: Parser.Root): Stats
 }
 
 /**
@@ -33,9 +33,9 @@ export interface StatsCollector {
 export function createStatsCollector(): StatsCollector {
   return {collect}
 
-  function collect(root: Root): Stats {
+  function collect(root: Parser.Root): Stats {
     const stats: Stats = {
-      tools: new Map<string, ToolShape>(),
+      tools: new Map<string, Parser.ToolShape>(),
       drillsPerTool: new Map<string, number>(),
       totalDrills: 0,
       minDrillSize: 999999,
@@ -44,10 +44,10 @@ export function createStatsCollector(): StatsCollector {
 
     let currentTool = ''
     for (let node of root.children) {
-      if (node instanceof ToolDefinition) {
+      if (node.type === Parser.TOOL_DEFINITION) {
         stats.tools.set(node.code, node.shape)
 
-        if (node.shape instanceof Circle) {
+        if (node.shape.type === Parser.CIRCLE) {
           const diameter = node.shape.diameter
           if (diameter < stats.minDrillSize) {
             stats.minDrillSize = diameter
@@ -56,9 +56,9 @@ export function createStatsCollector(): StatsCollector {
             stats.maxDrillSize = diameter
           }
         }
-      } else if (node instanceof ToolChange) {
+      } else if (node.type === Parser.TOOL_CHANGE) {
         currentTool = node.code
-      } else if (node instanceof InterpolateMode) {
+      } else if (node.type === Parser.INTERPOLATE_MODE) {
         if (currentTool == '') {
           throw new Error('no tool selected')
         }
