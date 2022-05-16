14a6cd473626817d4aef2caf0a2c62fbd8fbfff2
KN4CK3R <admin@oldschoolhack.me>
2022-05-16 17:23:09 +0000

    Convert to simple method.

--- packages/stats/src/collector.ts
+++ packages/stats/src/collector.ts
@@ -2,73 +2,58 @@ import * as Parser from '@tracespace/parser'
 import {Stats} from './types'
 
 /**
- * Board stats collector.
- *
- * @category StatsCollector
- */
-export interface StatsCollector {
-  /** Collect stats of a parsed AST */
-  collect(root: Parser.Root): Stats
-}
-
-/**
- * {@linkcode StatsCollector} factory and the primary export of the library.
+ * Method to collect stats of a parsed drill file.
  *
  * @example
  * ```ts
  * import {createParser} from '@tracespace/parser'
- * import {createStatsCollector} from '@tracespace/stats'
+ * import {collectStats} from '@tracespace/stats'
  *
  * const parser = createParser()
- * const statsCollector = createStatsCollector()
  *
  * parser.feed(...)
  *
  * const tree = parser.results()
- * const stats = statsCollector.collect(tree)
+ * const stats = collectStats(tree)
  * ```
  *
- * @category StatsCollector
+ * @category Stats
  */
-export function createStatsCollector(): StatsCollector {
-  return {collect}
-
-  function collect(root: Parser.Root): Stats {
-    const stats: Stats = {
-      tools: new Map<string, Parser.ToolShape>(),
-      drillsPerTool: new Map<string, number>(),
-      totalDrills: 0,
-      minDrillSize: 999999,
-      maxDrillSize: 0,
-    }
-
-    let currentTool = ''
-    for (const node of root.children) {
-      if (node.type === Parser.TOOL_DEFINITION) {
-        stats.tools.set(node.code, node.shape)
-
-        if (node.shape.type === Parser.CIRCLE) {
-          const diameter = node.shape.diameter
-          if (diameter < stats.minDrillSize) {
-            stats.minDrillSize = diameter
-          }
-          if (diameter > stats.maxDrillSize) {
-            stats.maxDrillSize = diameter
-          }
-        }
-      } else if (node.type === Parser.TOOL_CHANGE) {
-        currentTool = node.code
-      } else if (node.type === Parser.INTERPOLATE_MODE) {
-        if (currentTool === '') {
-          throw new Error('no tool selected')
-        }
-        const oldCount = stats.drillsPerTool.get(currentTool) ?? 0
-        stats.drillsPerTool.set(currentTool, oldCount + 1)
-
-        stats.totalDrills++
-      }
-    }
-
-    return stats
+export function collectStats(root: Parser.Root): Stats {
+  const stats: Stats = {
+    tools: new Map<string, Parser.ToolShape>(),
+    drillsPerTool: new Map<string, number>(),
+    totalDrills: 0,
+    minDrillSize: 999999,
+    maxDrillSize: 0,
   }
+
+  let currentTool = ''
+  for (const node of root.children) {
+    if (node.type === Parser.TOOL_DEFINITION) {
+      stats.tools.set(node.code, node.shape)
+
+      if (node.shape.type === Parser.CIRCLE) {
+        const diameter = node.shape.diameter
+        if (diameter < stats.minDrillSize) {
+          stats.minDrillSize = diameter
+        }
+        if (diameter > stats.maxDrillSize) {
+          stats.maxDrillSize = diameter
+        }
+      }
+    } else if (node.type === Parser.TOOL_CHANGE) {
+      currentTool = node.code
+    } else if (node.type === Parser.INTERPOLATE_MODE) {
+      if (currentTool === '') {
+        throw new Error('no tool selected')
+      }
+      const oldCount = stats.drillsPerTool.get(currentTool) ?? 0
+      stats.drillsPerTool.set(currentTool, oldCount + 1)
+
+      stats.totalDrills++
+    }
+  }
+
+  return stats
 }
